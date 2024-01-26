9bdefd4f546b5fcf5c9043c5868ecf3ba3eb3048
Vikashuvi <vikashuvi07@gmail.com>
2024-01-26 14:22:12 +0530

    more changes perfomed

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,34 +32,49 @@ $(document).ready(function() {
     nextMsg();
 
     // Add smooth scrolling to all links
-    $("#down").on('click', function(event) {
-        $("#go_down").hide();
-        if (this.hash !== "") {
+    $(document).ready(function(){
+        // Add smooth scrolling to all links
+        $("#down").on('click', function(event) {
+          $("#go_down").hide();
+      
+          // Make sure this.hash has a value before overriding default behavior
+          if (this.hash !== "") {
+            // Prevent default anchor click behavior
             event.preventDefault();
+      
+            // Store hash
             var hash = this.hash;
+      
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-                window.location.hash = hash;
+              scrollTop: $(hash).offset().top
+            }, 800, function(){
+              // Add hash (#) to URL when done scrolling (default click behavior)
+              window.location.hash = hash;
             });
-        }
-    });
-
-    function isScrolledIntoView(elem) {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-        docViewBottom -= 100; // offset of added
-        var elemTop = $(elem).offset().top;
-        var elemBottom = elemTop + $(elem).height();
-        return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom >= docViewBottom) && (elemTop >= docViewTop));
-    }
-
-
-    // Add vertical scrolling with mouse wheel
-    $('html, body').on('wheel', function(e) {
-        $('html, body').stop().animate({
-            scrollTop: $(window).scrollTop() - e.originalEvent.deltaY * 80
-        }, 800);
-        e.preventDefault();
-    });
+          } // End if
+        });
+      });
+      
+      function isScrolledIntoView(elem)
+      {
+          var docViewTop = $(window).scrollTop();
+          var docViewBottom = docViewTop + $(window).height();
+          docViewBottom -= 100; // offset of added
+      
+          var elemTop = $(elem).offset().top;
+          var elemBottom = elemTop + $(elem).height();
+      
+          return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom >= docViewBottom) && (elemTop >= docViewTop));
+      }
+      
+      $(window).scroll(function() {  
+          // alert("Working");  
+          if(isScrolledIntoView($('#obj2')))
+          {
+              $("#go_down").hide();
+          }    
+      });
+      
 });
